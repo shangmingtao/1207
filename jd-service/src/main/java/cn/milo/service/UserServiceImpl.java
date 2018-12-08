57632bb8a508b7c7303c258979ea5bc0ebeceb12
shangmingtao <shangmingtao@126.com>
2018-12-09 02:00:02 +0800

    300

--- jd-service/src/main/java/cn/milo/service/UserServiceImpl.java
+++ jd-service/src/main/java/cn/milo/service/UserServiceImpl.java
@@ -32,10 +32,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public cn.milo.dto.User searchUser(String id) {
-        log.info("service 被访问了。。。传入参数226：   " + id);
+        log.info("service 被访问了。。。传入参数300：   " + id);
         cn.milo.dto.User user1 = new cn.milo.dto.User();
         if (redisTemplateUtil.get(id) != null){
-            log.info("redis mingzhong le 226....");
+            log.info("redis mingzhong le 300....");
             user1.setName(redisTemplateUtil.get(id)+"");
             return user1;
         }else {
